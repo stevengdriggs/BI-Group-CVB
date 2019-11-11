03c18cbb6f7b818ba2b062ab2ec80cdefc5a6d0d
Jace Iverson <jaceiverson@Jaces-MacBook-Pro.local>
2019-11-11 12:50:36 -0700

    Adding all the files we worked on last week

---
+++ pritnerData.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#for simplisity, I printed most of the statments so we could see what they do.
+
+#you will need to change this to the file path that the file is saved.
+printers=pd.read_csv('~/Desktop/CVB/printers.csv')
+
+locIP=[]
+
+printers["IP Address"]
+
+#there was a weird empty last entry. this deletes it
+
+printers.drop(printers.tail(1).index,inplace=True)
+
+#creates a column named "Location IP", and then assigns the 3 octet in the IP address column to that value
+#we will take this and we will link this to another table
+def labelLocalIP(row):
+    return row.split('.')[2]
+printers['Location IP']=printers['IP Address'].apply(lambda row: labelLocalIP(row))
+
+#gets you: a unique attribute, all the columns in the dataframe, and a unique row
+print(printers['Location IP'][0], printers.columns, printers.iloc[0])
+
+#this gives the count of values in a specific column, in this case Location IP
+print(printers['Location IP'].value_counts())
+
+#write out to a csv
+#printers.to_csv('Matt.csv')
+
+
+
